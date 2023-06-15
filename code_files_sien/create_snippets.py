77c56142119b7bee823407bda5fb4f13da61ae60
sienjansen <s.sien.jansen@gmail.com>
2023-06-15 16:37:51 +0200

    shit

--- code_files_sien/create_snippets.py
+++ code_files_sien/create_snippets.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import random
 
-def create_snippets(file, snippet_length):
+def create_snippets(file, snippet_length, per_second):
     
+    """
+    
+    """
     df = pd.read_csv(file)
     final_data = pd.DataFrame(columns = list(df.columns)+["snippet"])
     groups = df.groupby("entry_num")
+    
 
-    # extract last 20 seconds snippet and random snippet
+    # extract last 20 seconds snippet and random snippet per group
     for _, group in groups:
         
         ## last 20 seconds:
@@ -22,49 +26,30 @@ def create_snippets(file, snippet_length):
 
         # add snippet to final dataset
         df_20sec = group.loc[indices]
-        final_data["snippet"] = "last_seconds"
-        final_data = pd.concat([final_data, df_20sec], axis=0)
+        df_20sec["snippet"] = "last_seconds"
+        final_data = pd.concat([final_data, df_20sec], axis = 0)
 
         # remove this snippet from the original dataset (to avoid double picking)
-        df.drop(indices, inplace = True)
-
-
+        group.drop(indices, inplace = True)
+        
         ## random snippet:
 
         # set last possible initial snippet point
         random_snippet_max = snippet_start - snippet_length
-        # print(random_snippet_max)
 
-        # create list of possible initial snippet points
+
         mask = group["Time (s)"] <= random_snippet_max
         indices = list(group.loc[mask].index)
-        # print(indices)
 
-        # set boundries of random snippet
         rand_start = random.choice(indices)
-        rand_end = rand_start + snippet_length
-
-        # print(rand_start)
-        # print(rand_end)
-
-        mask = (group["Time (s)"] > rand_start) & (group["Time (s)"] < rand_end)
-        indi = list(group.loc[mask].index)
-        print(indi)
-        # idk = group.loc[indices]
-        # print(idk)
-
+        rand_end = rand_start + (snippet_length * per_second)
+        
+        
+        random_20sec = group.loc[rand_start : rand_end-1]
+        random_20sec["snippet"] = "random_snippet"
+        random_20sec["fall_top"] = 0
+        final_data = pd.concat([final_data, random_20sec], axis = 0)
         
 
-    ### TODO: 
-        # Noteer in de dataframe dat de 'last_sec" snipplet is en niet 'random_snipplet'
-
-    ### TODO: 
-        # add a random snippet per group (zorg ervoor dat je weet waar het laatste punt is waarvan je mag nemen (dus 20
-        # sec voor het einde) en dan alles daarvoor mag je een random one kiezen en dan de volgende 20 sec nemen)
-        # pick random index in the group 
-        # Zorg dat de 'complete/fall' column nu in deze naar complete gaat omdat deze altijd gecompleet zijn
-
-
-# create_snippets("../datasets/cut_data/cut_data_sien.csv", 20)
-create_snippets("../datasets/raw_data_sien.csv", 20)
+create_snippets("../datasets/cut_data/cut_data_sien.csv", 20, 5)
 
