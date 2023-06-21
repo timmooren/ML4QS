8c2345341c2828c702a69a24d72fe6ab67bdc3e9
rnambaa <ryo.namba@gmail.com>
2023-06-21 18:47:38 +0200

    asdf

--- create_dataset.py
+++ create_dataset.py
@@ -70,9 +70,9 @@ def add_datetime(df):
         mask = df_copy["entry_num"] == entry_id
         df_copy.loc[mask, "datetime"] = datetime_sequence_dict[entry_id]
 
-    # Remove timezone information from datetime column
-    df_copy['datetime'] = pd.to_datetime(df_copy['datetime'])
-    df_copy['datetime'] = df_copy['datetime'].dt.tz_localize(None)
+    # # Remove timezone information from datetime column
+    # df_copy['datetime'] = pd.to_datetime(df_copy['datetime'])
+    # df_copy['datetime'] = df_copy['datetime'].dt.tz_localize(None)
 
     return df_copy
 
